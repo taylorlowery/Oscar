0467d6907cee9c635e51b89b798a3dc886ff6e02
Taylor Lowery <admiral.taylor@gmail.com>
2020-07-03 13:37:02 -0700

    created initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { TicketEditComponent } from './ticket-edit/ticket-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    HomeComponent,
+    DashboardComponent,
+    TicketsComponent,
+    TicketEditComponent
   ],
   imports: [
     BrowserModule
